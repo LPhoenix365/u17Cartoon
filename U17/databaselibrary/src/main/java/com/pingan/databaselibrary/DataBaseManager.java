37a3802c305cacf9307677d9b08f94163a889877
liupeng502 <ah_liupeng@163.com>
2017-04-13 18:18:42 +0800

    测试

---
+++ U17/databaselibrary/src/main/java/com/pingan/databaselibrary/DataBaseManager.java
@@ -0,0 +1,14 @@
+package com.pingan.databaselibrary;
+
+/**
+ * Description
+ *
+ * @author liupeng502
+ * @data 2017/4/13
+ */
+
+public class DataBaseManager {
+
+    public DataBaseManager() {
+    }
+}
