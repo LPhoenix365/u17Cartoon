1758a4e7022ee8ef2aca844467f66066a2ea5745
liup <823441568@qq.com>
2017-05-09 00:11:43 +0800

    实现动态加载

--- app/src/main/java/com/pingan/u17/ui/fragment/ChildRecommendFragment.java
+++ app/src/main/java/com/pingan/u17/ui/fragment/ChildRecommendFragment.java
@@ -20,6 +20,7 @@ import com.pingan.u17.R;
 import com.pingan.u17.base.BaseFragment;
 import com.pingan.u17.bean.HomePageBean;
 import com.pingan.u17.util.Constants;
+import com.pingan.u17.util.ToolUtils;
 
 import java.util.List;
 
@@ -34,7 +35,7 @@ public class ChildRecommendFragment extends BaseFragment {
     /*@BindView(R.id.rv_recommend)
     RecyclerView rvRecommend;*/
     @BindView(R.id.ll_container_recommend)
-    LinearLayout     llContainer;
+    LinearLayout llContainer;
 
     private HomePageBean mHomePageBean;
     private static final int THREE = 3;
@@ -104,7 +105,7 @@ public class ChildRecommendFragment extends BaseFragment {
                 switch (comicList.getComicType()) {
                     case THREE:
                         List<HomePageBean.DataBean.ReturnDataBean.ComicListsBean.ComicsBean> comics = comicList.getComics();
-                        addThreeModel(comics);
+                        addSixModel(comics);
                         break;
                     case FOUR:
 
@@ -113,7 +114,7 @@ public class ChildRecommendFragment extends BaseFragment {
 
                         break;
                     case SIX:
-                        
+
                         break;
                     case SEVEN:
 
@@ -142,17 +143,25 @@ public class ChildRecommendFragment extends BaseFragment {
      */
     private void addSixModel(List<HomePageBean.DataBean.ReturnDataBean.ComicListsBean.ComicsBean> comics) {
         if (comics != null && comics.size() > 0) {
+            LinearLayout modelLinearLayout = new LinearLayout(mActivity);
+            LinearLayout.LayoutParams modelLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            llContainer.addView(modelLinearLayout, modelLayoutParams);
+            LinearLayout linearLayout = null;
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             for (int i = 0; i < comics.size(); i++) {
-                LinearLayout modelLinearLayout = new LinearLayout(mActivity);
-                LinearLayout.LayoutParams modelLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                llContainer.addView(modelLinearLayout, modelLayoutParams);
-                LinearLayout linearLayout = null;
                 if (i % 3 == 0) {
                     linearLayout = new LinearLayout(mActivity);
+                    modelLinearLayout.addView(linearLayout,layoutParams);
                 }
                 View view = mInflater.inflate(R.layout.item_recomend_three, null, false);
                 SixViewHolder viewHolder = new SixViewHolder(view);
                 viewHolder.bindView(comics.get(i));
+                int screenDpi = ToolUtils.getScreenDpi(mActivity);
+                View boder = mInflater.inflate(R.layout.item_common_boder, null, false);
+                linearLayout.addView(boder);
+                int itemWidth = (screenDpi - boder.getWidth() * 4) / 3;
+                LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams(ToolUtils.dip2px(mActivity,itemWidth), ToolUtils.dip2px(mActivity,300));
+                linearLayout.addView(view, layoutParam);
             }
 
         }
@@ -174,12 +183,12 @@ public class ChildRecommendFragment extends BaseFragment {
         @BindView(R.id.tv_corner_seven)
         TextView         tvCornerSeven;
 
-        public SixViewHolder(View view){
-            ButterKnife.bind(this,view);
+        public SixViewHolder(View view) {
+            ButterKnife.bind(this, view);
         }
 
-        public void bindView(HomePageBean.DataBean.ReturnDataBean.ComicListsBean.ComicsBean comics){
-            FrescoImageUtil.displayImgFromNetwork(svCoverSeven,comics.getCover());
+        public void bindView(HomePageBean.DataBean.ReturnDataBean.ComicListsBean.ComicsBean comics) {
+            FrescoImageUtil.displayImgFromNetwork(svCoverSeven, comics.getCover());
             tvNameSeven.setText(comics.getAuthor_name());
             tvCornerSeven.setText(comics.getCornerInfo());
         }
