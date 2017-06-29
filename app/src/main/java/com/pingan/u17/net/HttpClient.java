8abc02c7e272f4ce126f226a7b02f31666231abc
liup <823441568@qq.com>
2017-06-30 00:32:00 +0800

    1

--- app/src/main/java/com/pingan/u17/net/HttpClient.java
+++ app/src/main/java/com/pingan/u17/net/HttpClient.java
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -57,7 +56,7 @@ public class HttpClient {
         okHttpClient = genericClient(builder);
         Retrofit retrofit = new Retrofit.Builder().baseUrl(builder.getHostIp())
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                //.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .client(okHttpClient)
                 .build();
         apiService = retrofit.create(builder.getRestfulApi());
