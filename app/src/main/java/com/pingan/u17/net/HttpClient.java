e959ac4751dac7364032cd2dd79243500d40500b
liupeng502 <ah_liupeng@163.com>
2017-07-21 18:12:52 +0800

    123

--- app/src/main/java/com/pingan/u17/net/HttpClient.java
+++ app/src/main/java/com/pingan/u17/net/HttpClient.java
@@ -4,7 +4,17 @@ import com.pingan.u17.base.U17Application;
 import com.pingan.u17.util.AppEnvConstants;
 
 import java.io.File;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
@@ -19,8 +29,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * @data 2017/5/31
  */
 public class HttpClient {
-    private        OkHttpClient okHttpClient;
-    private        Object       apiService;
+    private OkHttpClient okHttpClient;
+    private Object       apiService;
+    private HostnameVerifier hostnameVerifier;
+    private SSLContext sslContext;
 
 
     private Cache initCache() {
@@ -33,10 +45,10 @@ public class HttpClient {
 
 
     public RestApi getApiService() {
-        return (RestApi)apiService;
+        return (RestApi) apiService;
     }
 
-    public<T> T getApi(Class<T> api){
+    public <T> T getApi(Class<T> api) {
         return (T) apiService;
     }
 
@@ -62,6 +74,7 @@ public class HttpClient {
                 .build();
         apiService = retrofit.create(builder.getRestfulApi());
     }
+
     // 创建OkHttpClient实例
     private OkHttpClient genericClient(Builder builder) {
         OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder()
@@ -71,6 +84,65 @@ public class HttpClient {
                 .cache(builder.getCache())
                 .addInterceptor(builder.getInterceptor());
         return httpBuilder.build();
+    }
+
+
+    public void getUnsafeHttpClient() {
+        X509TrustManager[] trustAllCerts = new X509TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                            //校验客户端证书
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                            //校验服务端证书
+                        if (chain == null) {
+                            throw  new IllegalArgumentException("NUll Certs");
+                        }
+                        if (chain.length < 1) {
+                            throw  new IllegalArgumentException("CHeck Certs is empty");
+                        }
+                        for (X509Certificate x509Certificate : chain) {
+                            x509Certificate.checkValidity();
+                        }
+                    }
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        };
+
+        try {
+            sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, null);
+
+            //未真正校验服务器证书域名
+            hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    //未真正校验服务器证书域名
+                    HostnameVerifier hostnameVerifier = HttpsURLConnection.getDefaultHostnameVerifier();
+                    hostnameVerifier.verify("域名规则",session);
+                    return true;
+                }
+            };
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+        OkHttpClient build = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .hostnameVerifier(hostnameVerifier)
+                .sslSocketFactory(sslContext.getSocketFactory(), trustAllCerts[0])
+                .build();
+
     }
 
 
