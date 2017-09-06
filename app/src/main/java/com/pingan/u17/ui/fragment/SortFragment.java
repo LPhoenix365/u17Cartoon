f34e3f52ca4cc0d2777c1c237dd5afcf511597e0
liupeng502 <ah_liupeng@163.com>
2017-09-06 18:35:23 +0800

    12

--- app/src/main/java/com/pingan/u17/ui/fragment/SortFragment.java
+++ app/src/main/java/com/pingan/u17/ui/fragment/SortFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import com.pingan.u17.R;
 import com.pingan.u17.base.BaseFragment;
@@ -14,24 +15,31 @@ import com.pingan.u17.view.SortView;
 
 import java.util.HashMap;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Author：liupeng on 2017/2/24 09:55
  * Address：liupeng264@pingan.com.cn
  */
-public class SortFragment extends BaseFragment<SortView,SortPresenter> implements SortView{
+public class SortFragment extends BaseFragment<SortView, SortPresenter> implements SortView {
     public static final String TAG = SortFragment.class.getSimpleName();
+    @BindView(R.id.calendar)
+    CalendarView calendar;
+    Unbinder unbinder;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
     }
 
     @Override
@@ -64,5 +72,11 @@ public class SortFragment extends BaseFragment<SortView,SortPresenter> implement
     @Override
     public void showErrorMsg(String errormsg) {
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
